375ba29f905c3ababfdb414963e41769bb3d21ff
DAMIAKINTOMIDE <damiakintomide@gmail.com>
2019-04-07 16:04:26 +0100

    edit profile modal & change to draft page  (#36)
    * edit profile modal
    
    * changes to edit profile modal
    
    * Update draft.php

--- hng/includes/head.php
+++ hng/includes/head.php
@@ -13,6 +13,7 @@
 	<link href="https://fonts.googleapis.com/css?family=B612:700|Josefin+Sans:300" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
+    <link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/stylesheet.css">
 	<script src="bootstrap-3.3.7-dist/js/jquery-3.2.1.js"></script>
--- hng/includes/nav.php
+++ hng/includes/nav.php
@@ -6,7 +6,8 @@
         <h3>TONI ATUNRASE</h3>
         <h4 class="occupation">Web/Software Developer</h4>
         <p>Software Engineer at AWS design enthusiast and music lover</p>
-        <a href=""><i class="fa fa-paperclip"></i> hng/toni</a>
+        <a href=""><i class="fa fa-paperclip"></i> hng/toni</a> <br/>
+        <button type="button" data-toggle="modal" data-target="#edit-modal" class="btn btn-default rounded">Edit Profile</button>
       </div>
     </div>
     <hr>
@@ -21,3 +22,60 @@
     </div>
   </div>
 </div>
+
+
+
+<div class="modal fade" id="edit-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header border-0">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body p-3">
+                    
+                    <div class="row mt-3">
+                        <div class="form-group">
+                        <img src="images/fiverr.jpg" alt="" class="border-img  mx-40 img-circle">
+                         
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-xs-12">
+                            Name
+                        </div>
+                        <div class="col-xs-12">
+                        <input type="text" value="TONI ATUNRASE" class="form-control">
+                         
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-xs-12">
+                            Bio
+                        </div>
+                        <div class="col-xs-12">
+                        <textarea class="form-control" rows="3" cols="3">Software Engineer at AWS design enthusiast and music lover</textarea>
+                         
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-xs-12">
+                            Website
+                        </div>
+                        <div class="col-xs-12">
+                        <input type="text" value="www.hng.com/toni" class="form-control">
+                         
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-xs-12">
+                        <button type="button" class="btn btn-default rounded mx-40">save</button>
+                         
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+    </div> 
