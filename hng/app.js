0410cd0cb6a1c7dcddb27b7ca0823a1cf586c5b0
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-04-07 16:40:24 +0100

    Comment the code to make it clearer

--- hng/app.js
+++ hng/app.js
@@ -16,16 +16,18 @@ editor.addEventListener('focus', () => {
     }
 });
 
-// Focus on editor on tool click
+// Focus on editor on any tool click
 const toolbarTools = Array.prototype.slice.call(document.querySelectorAll('.toolbar .tool'));
 toolbarTools.forEach(el => el.addEventListener('click', () => editor.focus()));
 
+// When outside the editor is clicked set toolbar's opacity to 0
+// and if the editor is empty set its content to the default content.
 window.addEventListener('click', (e) => {
     let target = e.target;
     if (target !== editor && !toolbarTools.includes(target) && target !== toolbar) {
         toolbar.style.opacity = 0;
 
-        if (!editor.textContent.trim()) {
+        if (editor.textContent.trim()) {
             editor.textContent = editorDefaultText;
         }
     }
